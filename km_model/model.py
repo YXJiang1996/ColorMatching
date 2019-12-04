7bed5a3f6107b86a876f6657b37cc269691343cd
jyx <email@example.com>
2019-12-04 11:33:34 +0800

    训练数据生成修改

--- km_model/model.py
+++ km_model/model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import data
 
 
@@ -10,14 +11,17 @@ def main():
     bound = [0.1, 0.9, 0.1, 0.9]
     seed = 1  # seed for generating data
 
-    # generate data
+    # 生成训练数据
     concentrations, reflectance, x, info = data.generate(
         model=optical_model,
-        tot_dataset_size=2 ** 20 * 20,
+        total_dataset_size=2 ** 20 * 20,
         ydim=ydim,
         prior_bound=bound,
         seed=seed
     )
 
+    colors=np.arange(0,concentrations.shape[-1],1)
 
+    c_test=concentrations[-test_split]
+    r_test=reflectance[-test_split]
 main()
